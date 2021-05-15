c535ab761fe60d61fa499e99f8576e7443085ddf
Nikhil Shaw <69413813+nikhilshaw3@users.noreply.github.com>
2021-05-15 16:27:19 +0530

    Update CallScreen.js

--- screens/CallScreen.js
+++ screens/CallScreen.js
@@ -1,82 +1,76 @@
 import React, { Component } from "react";
-import {View,StyleSheet,Text,TextInput,Button,Linking,Platform} from "react-native";
-import {Header, Icon } from "react-native-elements";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  Linking,
+  Platform
+} from "react-native";
+import { Card, Header, Icon } from "react-native-elements";
+import MyHeader from '../components/MyHeader.js';
 import firebase from "firebase";
 import { RFValue } from "react-native-responsive-fontsize";
-import db from "../localdb.json";
+import db from "../config.js";
 import { SafeAreaProvider } from "react-native-safe-area-context";
  
 export default class App extends React.Component {
-constructor(props) {
-super(props);
-
-this.state = {
-mobileNo: "",
-text: '',
-country: ""
-};
-}
-
-call = (text) => {
-
-  var mobileNo = db[text]["mobileNo"]
-  var country=dictionary[text]["country"]
-
-  this.setState({
-
-    "mobileNo": mobileNo,
-    "country": country,
-})
-
-console.log("+++++++++callNumber ", this.state.mobileNo);
-
-let phoneNumber = this.state.mobileNo;
-
-if (Platform.OS !== "android") {
-phoneNumber = `telprompt:${this.state.mobileNo}`;
-} else {
-phoneNumber = `tel:${this.state.mobileNo}`;
-}
-
-Linking.canOpenURL(phoneNumber)
-.then(supported => {
-
-if (!supported) {
-Alert.alert("Number is not available");
-} else {
-return Linking.openURL(phoneNumber);
-}
-})
-.catch(err => console.log(err));
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileNo: ""
+    };
+  }
+  // Function to make phone call from React-native Application
+    call = () => {
+      console.log("+++++++++callNumber ", this.state.mobileNo);
+      let phoneNumber = this.state.mobileNo;
+      if (Platform.OS !== "android") {
+        phoneNumber = `telprompt:${this.state.mobileNo}`;
+      } else {
+        phoneNumber = `tel:${this.state.mobileNo}`;
+      }
+      Linking.canOpenURL(phoneNumber)
+        .then(supported => {
+          if (!supported) {
+            Alert.alert("Number is not available");
+          } else {
+            return Linking.openURL(phoneNumber);
+          }
+        })
+        .catch(err => console.log(err));
+    };
  
-render() {
-return (
-
-<View style={styles.container}>
+  render() {
+    return (
+      <View style={styles.container}>
 
 <Header
+
 leftComponent={<Icon name="arrow-left" type="feather" color="#ffffff" onPress={() => this.props.navigation.goBack()} />}
 centerComponent={{ text: "Make Call",style: {color: "#ffffff",fontSize: RFValue(20),fontWeight: "bold",},}}backgroundColor="red"
+
 />
         
-<Text style={{ textAlign: "center", fontSize: 20, paddingVertical: 30 }}>Enter the Number to make call</Text>
-        
-<TextInput
-value={this.state.mobileNo}
-onChangeText={mobileNo => this.setState({ mobileNo })}
-placeholder={"Enter the Emergency number ."}
-style={styles.input}
-keyboardType={"numeric"}
-/>
-
-<View style={{ marginTop: 20 }}>
-<Button onPress={this.call} title="Make phone call" />
-
-</View>
-</View>
-);
-}
+        <Text
+          style={{ textAlign: "center", fontSize: 20, paddingVertical: 30 }}
+        >
+          Enter the Number to make call
+        </Text>
+        <TextInput
+          value={this.state.mobileNo}
+          onChangeText={mobileNo => this.setState({ mobileNo })}
+          placeholder={"Enter the Emergency number ."}
+          style={styles.input}
+          keyboardType={"numeric"}
+        />
+        <View style={{ marginTop: 20 }}>
+          <Button onPress={this.call} title="Make phone call" />
+        </View>
+      </View>
+    );
+  }
 }
  
 const styles = StyleSheet.create({
